985a8eaf0dde9e6bf36614093e7fad62b886ede4
Sinclair Kinch <kinch.sk@pg.com>
2019-07-14 18:54:38 -0400

    first!

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Index = () => (
+    <div>
+      <p>Hey 👋</p>
+      <Link href="/linked-page"><button>I am a link.</button></Link>
+    </div>
+)
+  
+export default Index
---
+++ pages/linked-page.js
@@ -0,0 +1,7 @@
+const LinkedPage = () => (
+    <div>
+      <p>Hi It's Me, Another Page</p>
+    </div>
+)
+  
+export default LinkedPage
