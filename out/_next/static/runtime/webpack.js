25e9a15f9213f5489dda2d89711db51d29cf5d1b
Sinclair Kinch <kinch.sk@pg.com>
2019-07-14 22:08:30 -0400

    trying this link HOC

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "30b7c8c48016951c09f3";
+/******/ 	var hotCurrentHash = "43e26880926bbf8c68b0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
