e38794a712613a6c5111ba0297da1028f9151d63
Sinclair Kinch <kinch.sk@pg.com>
2019-07-15 00:02:10 -0400

    trying this

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     exportPathMap: function () {
         return {
-            '/': { page: '/' }
+            '/': { page: '/' },
+            '/linked-page': { page: '/linked-page'}
         }
-    },
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/personal' : '',
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Link from '../components/CustomLink';
+import Link from 'next/link'
 
 const Index = () => (
     <div>
