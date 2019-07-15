e6df2bd028d050c76449b258320f4245cbc71e61
Sinclair Kinch <kinch.sk@pg.com>
2019-07-14 22:16:44 -0400

    hmmmmmmm

--- components/CustomLink.js
+++ components/CustomLink.js
@@ -1,5 +1,10 @@
 import Link from 'next/link'
 
 export default ({href, children}) => (
-    <Link href={href} as={process.env.NODE_ENV === 'production' ? '/personal' : ''}>{children}</Link>
+    <Link 
+        href={href} 
+        as={`${process.env.NODE_ENV === 'production' ? '/personal/' : ''}${href}`}
+    >
+        {children}
+    </Link>
 )
