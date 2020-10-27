1cad4f5c2bf87f44c2000475e32b62fb3488902f
Sinclair Kinch <sinclair.kinch@sogeti.com>
2020-10-27 04:28:01 -0400

    lil css tweaks

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d7a24b5bca95da027d3a";
+/******/ 	var hotCurrentHash = "58000f4b82e68c8bcb66";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
