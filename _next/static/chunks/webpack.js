5133738060bda5b3eb55634019f257cc5f1594ca
Sinclair Kinch <sinclair.kinch@sogeti.com>
2020-10-27 04:31:17 -0400

    pink links

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "58000f4b82e68c8bcb66";
+/******/ 	var hotCurrentHash = "5e07cf076900b98326bc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
