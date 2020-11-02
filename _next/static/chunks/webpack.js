eb685749e1e838f8fb599f09d874bdb031cc8b4b
Sinclair Kinch <sinclair.kinch@sogeti.com>
2020-11-02 12:13:55 -0500

    fix font flash

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5e07cf076900b98326bc";
+/******/ 	var hotCurrentHash = "e52d3fe306a8fd53056e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
