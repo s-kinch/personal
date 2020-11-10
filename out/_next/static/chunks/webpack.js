12eb5288b0e4638a873951c9059ef0e1e647524f
Sinclair Kinch <sinclair.kinch@sogeti.com>
2020-11-10 04:06:16 -0500

    update styles and xword #3

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e52d3fe306a8fd53056e";
+/******/ 	var hotCurrentHash = "2c23b685056f818bf37d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
